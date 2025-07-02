11fbd9a50c61d9a27a8e926beb095dff2ed72ddc
Luis <108845020+xiLuiis@users.noreply.github.com>
2025-07-02 00:18:07 -0600

    feat: add per-user custom messages and flexible permissions system

--- src/main/java/com/xiluiis/service/PlayerRankService.java
+++ src/main/java/com/xiluiis/service/PlayerRankService.java
@@ -2,9 +2,16 @@ package com.xiluiis.service;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerRankService implements RankService {
+    private final JavaPlugin plugin;
+    
+    public PlayerRankService(JavaPlugin plugin){
+        this.plugin = plugin;
+    }
 
+    @Override
     public String getPlayerRank(Player player){
         if(player.hasPermission("welcometitles.vip")){
             return "vip";
@@ -19,8 +26,56 @@ public class PlayerRankService implements RankService {
         }
         return "default";
     }
- 
+
+    @Override
     public boolean isAllowedToSet(CommandSender sender){
-        return sender.hasPermission("welcometitles.vip") ||  sender.hasPermission("welcometitles.admin") || sender.isOp();
+        //sender.hasPermission("welcometitles.vip") ||  sender.hasPermission("welcometitles.admin") || sender.isOp();
+        Boolean adminAllowed = plugin.getConfig().getBoolean("sub-command-permissions.set.admin");
+        Boolean modAllowed = plugin.getConfig().getBoolean("sub-command-permissions.set.mod");
+        Boolean vipAllowed = plugin.getConfig().getBoolean("sub-command-permissions.set.vip");
+        Boolean defaultAllowed = plugin.getConfig().getBoolean("sub-command-permissions.set.default");
+
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+
+            if(player.isOp()){
+                return true;
+            }
+
+            String playerRank = getPlayerRank(player);
+            
+            Boolean hasPermissions =    (playerRank.equals("vip")&&vipAllowed) ? true :
+                                        (playerRank.equals("admin")&&adminAllowed) ? true :
+                                        (playerRank.equals("mod")&&modAllowed) ? true :
+                                        (playerRank.equals("default")&&defaultAllowed) ? true : false;
+
+            return hasPermissions;
+        }else return false;
+    }
+
+    @Override
+    public boolean isAllowedToHelp(CommandSender sender){
+        //sender.hasPermission("welcometitles.vip") ||  sender.hasPermission("welcometitles.admin") || sender.isOp();
+        Boolean adminAllowed = plugin.getConfig().getBoolean("sub-command-permissions.help.admin");
+        Boolean modAllowed = plugin.getConfig().getBoolean("sub-command-permissions.help.mod");
+        Boolean vipAllowed = plugin.getConfig().getBoolean("sub-command-permissions.help.vip");
+        Boolean defaultAllowed = plugin.getConfig().getBoolean("sub-command-permissions.help.default");
+
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+
+            if(player.isOp()){
+                return true;
+            }
+
+            String playerRank = getPlayerRank(player);
+            
+            Boolean hasPermissions =    (playerRank.equals("vip")&&vipAllowed) ? true :
+                                        (playerRank.equals("admin")&&adminAllowed) ? true :
+                                        (playerRank.equals("mod")&&modAllowed) ? true :
+                                        (playerRank.equals("default")&&defaultAllowed) ? true : false;
+
+            return hasPermissions;
+        }else return false;
     }
 }
